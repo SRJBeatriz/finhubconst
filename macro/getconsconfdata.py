7ecd8bde21f5723c18bb88a06f123d4e3617658e
beatrizsj <srjbeatriz@gmail.com>
2019-07-16 16:34:02 -0300

    New18:41

---
+++ macro/getconsconfdata.py
@@ -0,0 +1,45 @@
+"""
+Authors: Helena Bianchessi and Beatriz Jesus
+"""
+from bloomberg import BBG
+import pandas as pd
+from sklearn import preprocessing
+import numpy as np
+import matplotlib.pyplot as plt
+
+bbg = BBG()
+
+# Conference Board Consumer Confidence SA 1985=100
+#   Original Date: '28-fev-1967'
+
+start_date = pd.to_datetime('30-jan-2015')
+end_date = pd.to_datetime('15-jun-2019')
+
+df = bbg.fetch_series(securities=['CONCCONF Index'],
+                      fields=['PX_LAST'],
+                      startdate=start_date,
+                      enddate=end_date)
+
+concconf = pd.DataFrame(data=df)
+concconf = concconf.droplevel(0)
+concconf = concconf.reset_index()
+concconf = concconf.set_index('TRADE_DATE')
+concconf = concconf.resample('Q').mean()
+
+x = np.array(concconf['CONCCONF Index'])
+ccnorm = preprocessing.normalize([x])
+ccnorm = ccnorm.reshape(-1,1)
+
+confnorm = concconf
+confnorm['CONCCONF Index Normalized'] = ''
+confnorm['CONCCONF Index Normalized'] = ccnorm
+confnorm.info()
+
+#print(ccnorm)
+#print(confnorm)
+
+ax = plt.gca()
+#concconf.plot(kind='line', color='blue', ax=ax)
+confnorm.plot(kind='line', y='CONCCONF Index Normalized', color='green', ax=ax)
+
+plt.show()
---
+++ macro/getvol90data.py
@@ -0,0 +1,61 @@
+"""
+Authors: Helena Bianchessi and Beatriz Jesus
+"""
+from bloomberg import BBG
+import pandas as pd
+from sklearn import preprocessing
+import numpy as np
+import matplotlib.pyplot as plt
+
+bbg = BBG()
+
+# Pulling IBOVESPA and S&P indexes volatility
+#   Original Date: '28-fev-1967'
+
+start_date = pd.to_datetime('30-jan-2015')
+end_date = pd.to_datetime('today')
+
+df = bbg.fetch_series(securities=['SPX Index', 'IBOV Index'],
+                      fields=['VOLATILITY_90D'],
+                      startdate=start_date,
+                      enddate=end_date)
+
+volSPX_90 = pd.DataFrame(data=df['SPX Index'])
+volSPX_90 = volSPX_90.droplevel('FIELD')
+volSPX_90 = volSPX_90.resample('Q').mean()
+
+volIBOV_90 = pd.DataFrame(data=df['IBOV Index'])
+volIBOV_90 = volIBOV_90.droplevel('FIELD')
+volIBOV_90 = volIBOV_90.resample('Q').mean()
+
+ax = plt.gca()
+
+volSPX_90.plot(kind='line', color='blue', ax=ax)
+volIBOV_90.plot(kind='line', color='green', ax=ax)
+
+#plt.show()
+
+# Normalized series SPX
+
+x = np.array(volSPX_90['SPX Index'])
+spxnorm = preprocessing.normalize([x])
+spxnorm = spxnorm.reshape(-1,1)
+
+volnormSPX = volSPX_90
+volnormSPX['SPX Index Normalized'] = ''
+volnormSPX['SPX Index Normalized'] = spxnorm
+
+# Normalized series IBOV
+
+y = np.array(volIBOV_90['IBOV Index'])
+ibovnorm = preprocessing.normalize([y])
+ibovnorm = ibovnorm.reshape(-1,1)
+
+volnormIBOV = volIBOV_90
+volnormIBOV['IBOV Index Normalized'] = ''
+volnormIBOV['IBOV Index Normalized'] = ibovnorm
+
+# Info
+
+volnormSPX.info()
+volnormIBOV.info()
