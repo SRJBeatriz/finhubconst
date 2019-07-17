04b5be5cc1c09c2c482fd7ecaa42070a527950c7
beatrizsj <srjbeatriz@gmail.com>
2019-07-17 15:15:53 -0300

    mudancas gerais

--- macro/getvol90data.py
+++ macro/getvol90data.py
@@ -8,11 +8,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 bbg = BBG()
+min_max_scaler = preprocessing.MinMaxScaler()
 
 # Pulling IBOVESPA and S&P indexes volatility
 #   Original Date: '28-fev-1967'
 
-start_date = pd.to_datetime('30-jan-2015')
+start_date = pd.to_datetime('01-jan-2010')
 end_date = pd.to_datetime('today')
 
 df = bbg.fetch_series(securities=['SPX Index', 'IBOV Index'],
@@ -28,34 +29,45 @@ volIBOV_90 = pd.DataFrame(data=df['IBOV Index'])
 volIBOV_90 = volIBOV_90.droplevel('FIELD')
 volIBOV_90 = volIBOV_90.resample('Q').last()
 
-ax = plt.gca()
+#ax = plt.gca()
 
-volSPX_90.plot(kind='line', color='blue', ax=ax)
-volIBOV_90.plot(kind='line', color='green', ax=ax)
+#volSPX_90.plot(kind='line', color='blue', ax=ax)
+#volIBOV_90.plot(kind='line', color='green', ax=ax)
 
-plt.show()
+#plt.show()
 
 # Normalized series SPX
 
 x = np.array(volSPX_90['SPX Index'])
-spxnorm = preprocessing.normalize([x])
-spxnorm = spxnorm.reshape(-1,1)
+x = x.reshape(-1,1)
+spxnorm = min_max_scaler.fit_transform(x)
 
 volnormSPX = volSPX_90
 volnormSPX['SPX Index Normalized'] = ''
 volnormSPX['SPX Index Normalized'] = spxnorm
+volnormSPX = volnormSPX.drop('SPX Index', axis=1)
 
 # Normalized series IBOV
 
 y = np.array(volIBOV_90['IBOV Index'])
-ibovnorm = preprocessing.normalize([y])
-ibovnorm = ibovnorm.reshape(-1,1)
+y = y.reshape(-1,1)
+ibovnorm = min_max_scaler.fit_transform(y)
 
 volnormIBOV = volIBOV_90
 volnormIBOV['IBOV Index Normalized'] = ''
 volnormIBOV['IBOV Index Normalized'] = ibovnorm
+volnormIBOV = volnormIBOV.drop('IBOV Index', axis=1)
 
 # Info
 
 volnormSPX.info()
 volnormIBOV.info()
+
+# Plotting
+
+ax = plt.gca()
+
+volnormSPX.plot(kind='line', y='SPX Index Normalized', color='blue', ax=ax)
+volnormIBOV.plot(kind='line', y='IBOV Index Normalized', color='green', ax=ax)
+
+plt.show()
